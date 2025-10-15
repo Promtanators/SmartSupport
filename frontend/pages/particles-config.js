a0a5930c188dec4e5439760e395fb10e6dde2865
Yegor <ef188209@gmail.com>
2025-10-15 04:46:09 +0300

    feat: added two very beautiful pages

---
+++ frontend/pages/particles-config.js
@@ -0,0 +1,64 @@
+const particlesConfig = {
+  background: {
+    color: {
+      value: "#f0f2f5",
+    },
+  },
+  fpsLimit: 120,
+  interactivity: {
+    events: {
+      onHover: {
+        enable: true,
+        mode: "repulse",
+      },
+      resize: true,
+    },
+    modes: {
+      repulse: {
+        distance: 80,
+        duration: 0.4,
+      },
+    },
+  },
+  particles: {
+    color: {
+      value: "#1677ff",
+    },
+    links: {
+      color: "#85a5ff",
+      distance: 150,
+      enable: true,
+      opacity: 0.5,
+      width: 1,
+    },
+    move: {
+      direction: "none",
+      enable: true,
+      outModes: {
+        default: "bounce",
+      },
+      random: false,
+      speed: 1.5,
+      straight: false,
+    },
+    number: {
+      density: {
+        enable: true,
+        area: 800,
+      },
+      value: 80,
+    },
+    opacity: {
+      value: 0.5,
+    },
+    shape: {
+      type: "circle",
+    },
+    size: {
+      value: { min: 1, max: 5 },
+    },
+  },
+  detectRetina: true,
+};
+
+export default particlesConfig;
